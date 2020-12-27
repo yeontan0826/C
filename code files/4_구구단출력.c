7d421bcde7225d1a97d869dbcc1b3e0308f0c285
yeontan0826 <yeontan0826@gmail.com>
2020-12-28 03:32:53 +0900

    4_구구단출력 소스파일 생성

---
+++ code files/4_구구단출력.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("%d더하기 %d는 %d입니다.\n", 2, 3, 5);
+	printf("%d빼기 %d는 %d입니다.\n", 5, 3, 2);
+	printf("%d곱하기 %d는 %d입니다.\n", 2, 3, 6);
+	printf("%d나누기 %d는 %d입니다.\n", 6, 3, 2);
+
+	return 0;
+}
