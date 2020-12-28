08b4b6d63b3c8ddb683b65cf5b7240a43da27687
yeontan0826 <yeontan0826@gmail.com>
2020-12-28 14:18:52 +0900

    10_자료형크기출력 소스파일 생성

---
+++ code files/10_자료형크기출력.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("char \t \t-> %d\n", sizeof(char));
+	printf("short \t \t-> %d\n", sizeof(short));
+	printf("int \t \t-> %d\n", sizeof(int));
+	printf("long \t \t-> %d\n", sizeof(long));
+	printf("long long \t-> %d\n", sizeof(long long));
+	printf("foat \t \t-> %d\n", sizeof(float));
+	printf("double \t \t-> %d\n", sizeof(double));
+	printf("long double \t-> %d\n", sizeof(long double));
+
+	return 0;
+}
