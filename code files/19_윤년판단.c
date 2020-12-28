ac775872843713506f4bbc51252bfc26d9978f5d
yeontan0826 <yeontan0826@gmail.com>
2020-12-28 17:22:48 +0900

    19_윤년판단 소스파일 생성

---
+++ code files/19_윤년판단.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int year;
+
+	printf("연도를 입력하시오(YYYY) : ");
+	scanf("%d", &year);
+
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+		printf("%d년은 윤년입니다.\n", year);
+	}
+	else {
+		printf("%d년은 윤년이 아닙니다.\n", year);
+	}
+
+	return 0;
+}
