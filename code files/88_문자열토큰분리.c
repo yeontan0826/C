d902eeccf301453147e7dd93db0dcaccfa49c44c
yeontan0826 <yeontan0826@gmail.com>
2021-01-11 19:31:45 +0900

    88_문자열토크분리.c

---
+++ code files/88_문자열토큰분리.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+char a[50] = "Man is immortal, because he has a soul";
+char seps[] = " ,\t\n";
+char* token, * context;
+
+int main() {
+
+	token = strtok_s(a, seps, &context);
+
+	while (token != NULL) {
+		printf("ÅäÅ« : %s\n", token);
+		token = strtok_s(NULL, seps, &context);
+	}
+
+	return 0;
+}
