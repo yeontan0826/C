4c679276f8ab9b6e75c56aebf78380f63e2d5a60
yeontan0826 <yeontan0826@gmail.com>
2021-01-14 21:38:24 +0900

    100_좌표일치판단 소스파일 생성

---
+++ code files/100_좌표일치판단.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+typedef struct {
+	int x;
+	int y;
+} POINT;
+
+int main() {
+
+	POINT p1 = { 3,4 };
+	POINT p2 = { 3,4 };
+
+	if (equal(&p1, &p2)) {
+		printf("두 점은 같습니다.");
+	} else {
+		printf("두 점은 다릅니다.");
+	}
+
+	return 0;
+}
+
+int equal(POINT* p1, POINT* p2) {
+
+	if (p1->x == p2->x && p1->y == p2->y) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
